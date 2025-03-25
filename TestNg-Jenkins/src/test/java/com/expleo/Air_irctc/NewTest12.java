c5a22c15fd7d3fced46c7bfc9c786212d9c37b49
SARANYA ALAGESAN <114178067+Saranyaa5@users.noreply.github.com>
2025-03-25 10:25:54 +0530

    Update NewTest12.java

--- TestNg-Jenkins/src/test/java/com/expleo/Air_irctc/NewTest12.java
+++ TestNg-Jenkins/src/test/java/com/expleo/Air_irctc/NewTest12.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -21,41 +23,47 @@ public class NewTest12 {
 
     @Test
     public void Userlogin() {
-//        driver.findElement(By.xpath("//div[@class=\"oxd-input-group oxd-input-field-bottom-space\"]//div[2]//input")).sendKeys("Admin");
-//        driver.findElement(By.xpath("//div[@class=\"oxd-input-group oxd-input-field-bottom-space\"]//div[2]//input[@name=\"password\"]")).sendKeys("admin123");
-//        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
-    	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement username = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='username']")));
         username.sendKeys("Admin");
-    	//driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
-    	driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
-    	driver.findElement(By.xpath("//button[@type='submit']")).click();
 
-    	// Wait for the logout button to be present
-    	WebElement logoutButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[3]/ul/li/span")));
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
 
-    	// Click the logout button
-    	logoutButton.click();
+        // Wait for the logout button to be present
+        WebElement logoutButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[3]/ul/li/span")));
 
-    	// Wait for the logout link to appear
-    	WebElement logoutLink = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'Logout')]")));
-    	//WebElement logoutLink = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[contains(text(),'Logout')]")));
+        // Click the logout button
+        logoutButton.click();
 
-    	// Get the actual text of the logout link
-    	String actual = logoutLink.getText();
+        // Wait for the logout link to appear
+        WebElement logoutLink = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'Logout')]")));
 
-    	// Expected text
-    	String expected = "Logout";
+        // Get the actual text of the logout link
+        String actual = logoutLink.getText();
+
+        // Expected text
+        String expected = "Logout";
         Assert.assertEquals(actual, expected, "Login not successful!");
     }
 
-    @Parameters("browser")
+    @Parameters({"browser", "headless"})
     @BeforeTest
-    public void beforeTest(@Optional("chrome") String browser) {
+    public void beforeTest(@Optional("chrome") String browser, @Optional("false") String headless) {
+        boolean isHeadless = Boolean.parseBoolean(headless);
+
         if (browser.equalsIgnoreCase("chrome")) {
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            if (isHeadless) {
+                options.addArguments("--headless=new"); // Use new headless mode
+            }
+            driver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("edge")) {
-            driver = new EdgeDriver();
+            EdgeOptions options = new EdgeOptions();
+            if (isHeadless) {
+                options.addArguments("--headless"); // Enable headless mode for Edge
+            }
+            driver = new EdgeDriver(options);
         } else {
             throw new IllegalArgumentException("Invalid browser name: " + browser);
         }
@@ -66,26 +74,10 @@ public class NewTest12 {
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
     }
 
-
     @AfterTest
     public void afterTest() {
         if (driver != null) {
-            driver.quit(); 
+            driver.quit();
         }
     }
 }
-
-//
-//<test name="ChromeTest">
-//<parameter name="browser" value="chrome"/>
-//<classes>
-//    <class name="com.expleo.Air_irctc.NewTest12"/>
-//</classes>
-//</test>
-//
-//<test name="EdgeTest">
-//<parameter name="browser" value="edge"/>
-//<classes>
-//    <class name="com.expleo.Air_irctc.NewTest12"/>
-//</classes>
-//</test>
